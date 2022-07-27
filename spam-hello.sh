b87976f9b4f8d68e97179ea6b1e6e08f8a974b4b
twcrone <twcrone@gmail.com>
2022-07-27 16:29:44 -0400

    Update shell script to iterate

--- spam-hello.sh
+++ spam-hello.sh
@@ -1,40 +1,5 @@
+for i in {1..25};
+do
 http localhost:8080/jee9/hello
 sleep 1
-http localhost:8080/jee9/hello
-sleep 1
-http localhost:8080/jee9/hello
-sleep 1
-http localhost:8080/jee9/hello
-sleep 1
-http localhost:8080/jee9/hello
-sleep 1
-http localhost:8080/jee9/hello
-sleep 1
-http localhost:8080/jee9/hello
-sleep 1
-http localhost:8080/jee9/hello
-sleep 1
-http localhost:8080/jee9/hello
-sleep 1
-http localhost:8080/jee9/hello
-sleep 1
-http localhost:8080/jee9/hello
-sleep 1
-http localhost:8080/jee9/hello
-sleep 1
-http localhost:8080/jee9/hello
-sleep 1
-http localhost:8080/jee9/hello
-sleep 1
-http localhost:8080/jee9/hello
-sleep 1
-http localhost:8080/jee9/hello
-sleep 1
-http localhost:8080/jee9/hello
-sleep 1
-http localhost:8080/jee9/hello
-sleep 1
-http localhost:8080/jee9/hello
-sleep 1
-http localhost:8080/jee9/hello
-sleep 1
+done
