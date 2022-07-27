e946f2f070f5a25c43caad6acd4e3a94e8f4e1d5
twcrone <twcrone@gmail.com>
2022-07-27 13:40:36 -0400

    Initial commit

---
+++ src/main/java/twcrone/jee9/HelloServlet.java
@@ -0,0 +1,17 @@
+package twcrone.jee9;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getOutputStream().println("Hello World");
+    }
+}
